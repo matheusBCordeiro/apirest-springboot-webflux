94ef53ad8ada7bd73f8100348832157f9181e429
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-04-26 20:12:13 -0300

    Service

---
+++ src/main/java/com/apirest/webflux/services/InterfacePlaylistService.java
@@ -0,0 +1,12 @@
+package com.apirest.webflux.services;
+
+import com.apirest.webfluxdocument.Playlist;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface InterfacePlaylistService {
+	Flux<Playlist> findAll();
+	Mono<Playlist> findById(String id);
+	Mono <Playlist> save(Playlist playlist);
+}
